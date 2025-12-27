4cf0e50e65f0067405714af46e7caeac4683559c
Gyula Madarasz <gyula.madarasz@pm.me>
2025-12-27 19:08:36 +0000

    some dependency fixes

--- dependencies/stevengj/nlopt/NloptDependency.cpp
+++ dependencies/stevengj/nlopt/NloptDependency.cpp
@@ -1,44 +1,62 @@
-#include "../../../Dependency.hpp"
 #include "../../../../cpptools/misc/EXTERN.hpp"
+#include "../../../../cpptools/misc/GithubDependency.hpp"
 #include "../../../../cpptools/misc/file_exists.hpp"
 #include "../../../../cpptools/misc/Executor.hpp"
 #include "../../../../cpptools/misc/ConsoleLogger.hpp"
 
 using namespace std;
 
-class NloptDependency: public Dependency {
+class NloptDependency: public GithubDependency {
 public:
-    NloptDependency(): Dependency() {}
+    NloptDependency(): GithubDependency() {}
     virtual ~NloptDependency() {}
 
-    void install(const string VERSION) override {
-        createLogger<ConsoleLogger>();
-
-        // Install and build nlopt with the selected version (if not already)
-        const string TARGET = fix_path(LIBS_DIR + "/stevengj/nlopt/");
-        const string BASE = fix_path(LIBS_DIR + "/..");
-        const string nloptf = TARGET + "/" + VERSION + "/build/nlopt.hpp";
-        LOG("Checking nlopt installed: " + nloptf);
-        if (!file_exists(nloptf)) { // already instelled?
-            LOG("=> not found, install nlopt...");
-            const string cmd = __DIR__ + "/install.sh " + LIBS_DIR + "/.. " + TARGET + " " + VERSION;
-            LOG("Execute: " + cmd);
-            Executor::execute(cmd);
-        }
+    void setVersion(string VERSION) override {
+        GithubDependency::setVersion(VERSION);
+        GithubDependency::setRepo("stevengj/nlopt");
     }
+
+    // void setVersion(string VERSION) override {
+    //     GithubDependency::setVersion(VERSION);
+    //     this->REPO = "nlohmann/json";
+    //     this->TARGET = fix_path(LIBS_DIR + "/" + REPO + "/");
+    // }
+
+    // void install() override {
+    //     createLogger<ConsoleLogger>();
+
+    //     // Install and build nlopt with the selected version (if not already)
+    //     const string TARGET = fix_path(LIBS_DIR + "/stevengj/nlopt/");
+    //     const string BASE = fix_path(LIBS_DIR + "/..");
+    //     const string nloptf = TARGET + "/" + VERSION + "/build/nlopt.hpp";
+    //     LOG("Checking nlopt installed: " + nloptf);
+    //     if (!file_exists(nloptf)) { // already instelled?
+    //         LOG("=> not found, install nlopt...");
+    //         const string cmd = __DIR__ + "/install.sh " + LIBS_DIR + "/.. " + TARGET + " " + VERSION;
+    //         LOG("Execute: " + cmd);
+    //         Executor::execute(cmd);
+    //     }
+    // }
 
     vector<string> flags() override {
         return { };
     }
 
     vector<string> libs() override {
-        return { };
+        const string path = getPath();
+        return {
+            path + "/build/libnlopt.a",
+            "-lm"  // NLopt uses math functions (sqrt, log, pow, etc.)
+        };
     }
 
     vector<string> incs() override {
+        const string path = getPath();
         return {
-            "libs/stevengj/nlopt/v2.10.1/build",
-            "libs/stevengj/nlopt/v2.10.1/src/api"
+            path + "/build",
+            path + "/src/api",
+            // "libs/stevengj/nlopt/v2.10.1/build",
+            // "libs/stevengj/nlopt/v2.10.1/src/api"
         };
     }
 };
--- dependencies/stevengj/nlopt/_install.sh
+++ dependencies/stevengj/nlopt/_install.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
 # Get parameters
-REPO="$1"
-BASE="$2"
-TARGET="$3"
-VERSION="$4"
+BASE="$1"
+TARGET="$2"
+VERSION="$3"
 
 # Validate input
 if [ -z "$BASE" ] || [ -z "$TARGET" ] || [ -z "$VERSION" ]; then
-    echo "Usage: $0 <github-repo> <base-folder> <target-folder> <version>"
+    echo "Usage: $0 <base-folder> <target-folder> <version>"
     exit 1
 fi
 
 # Move into BASE directory
-echo "Going to base folder: $BASE"
 cd "$BASE" || exit 1
 
 # Create directory if it does not exist
-echo "Creating lib folder: $TARGET/$VERSION"
 mkdir -p "$TARGET/$VERSION"
 
 # Clone repository into that directory
-echo "Cloning repository: https://github.com/$REPO.git to $TARGET/$VERSION"
-git clone "https://github.com/$REPO.git" "$TARGET/$VERSION"
+git clone https://github.com/stevengj/nlopt.git "$TARGET/$VERSION"
 
 # Checkout the requested version
-echo "Going to folder: $TARGET/$VERSION and checkout $VERSION"
 cd "$TARGET/$VERSION" || exit 1
 git checkout "$VERSION"
 
 # Build
-echo "Building..."
 mkdir build
 cd build || exit 1
 cmake -DBUILD_SHARED_LIBS=OFF ..
