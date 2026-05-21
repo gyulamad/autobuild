394886b4b49f6e11f660ddc29d4006e44ef4b0b7
Gyula Madarasz <gyula.madarasz@pm.me>
2026-05-21 07:58:19 +0100

    imtui dep added

---
+++ dependencies/ggerganov/imtui/ImtuiDependency.cpp
@@ -0,0 +1,41 @@
+#include "../../../../cpptools/misc/EXTERN.hpp"
+#include "../../../../cpptools/misc/file_exists.hpp"
+#include "../../../../cpptools/misc/GithubDependency.hpp"
+
+using namespace std;
+
+class ImtuiDependency: public GithubDependency {
+public:
+    using GithubDependency::GithubDependency;
+    virtual ~ImtuiDependency() {}
+
+    void setVersion(string VERSION) override {
+        GithubDependency::setVersion(VERSION);
+        GithubDependency::setRepo("ggerganov/imtui");
+    }
+
+    vector<string> flags() override {
+        return {};
+    }
+
+    vector<string> libs() override {
+        const string path = getPath();
+        return {
+            path + "/build/src/libimtui-ncurses.a",
+            path + "/build/src/libimtui.a",
+            path + "/build/third-party/libimgui-for-imtui.a",
+            "-lncurses",
+            "-ldl"
+        };
+    }
+
+    vector<string> incs() override {
+        const string path = getPath();
+        return {
+            path + "/include",
+            path + "/third-party/imgui"
+        };
+    }
+};
+
+EXTERN_GITHUB_DEPENDENCY(ImtuiDependency)
