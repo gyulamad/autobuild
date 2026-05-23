1f4d1b4adeb41c83ee4b422615f6b97e9bc37691
Gyula Madarasz <gyula.madarasz@pm.me>
2026-05-23 15:42:55 +0100

    cpp-tui lib added

---
+++ dependencies/jonoton/cpp-tui/CppTuiDependency.cpp
@@ -0,0 +1,34 @@
+#include "../../../../cpptools/misc/EXTERN.hpp"
+#include "../../../../cpptools/misc/file_exists.hpp"
+#include "../../../../cpptools/misc/GithubDependency.hpp"
+
+using namespace std;
+
+class CppTuiDependency: public GithubDependency {
+public:
+    using GithubDependency::GithubDependency;
+    virtual ~CppTuiDependency() {}
+
+    void setVersion(string VERSION) override {
+        GithubDependency::setVersion(VERSION);
+        GithubDependency::setRepo("jonoton/cpp-tui");
+    }
+
+    vector<string> flags() override {
+        return {};
+    }
+
+    vector<string> libs() override {
+        // Header-only library, no libraries to link
+        return {};
+    }
+
+    vector<string> incs() override {
+        const string path = getPath();
+        return {
+            path
+        };
+    }
+};
+
+EXTERN_GITHUB_DEPENDENCY(CppTuiDependency)
